afe762c006a003e4d930f02fa478fb61936e65f3
9514y <yaramalaharsha2000@gmail.com>
2025-07-14 12:17:51 -0400

    Initial commit

--- hosting.sh
+++ hosting.sh
@@ -1,4 +1,4 @@
- html_dri="/Users/h9514/Documents/Project"
-cd "$html_dri" || { echo "Directory not found"; exit 1; }
+html_dir="/Users/h9514/Documents/Project/web_hosting/"
+cd "$html_dir" || { echo "Directory not found"; exit 1; }
 echo "Hosting website at http://localhost:8080"
 http-server -p 8080
